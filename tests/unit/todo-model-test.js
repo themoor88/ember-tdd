c196b027f886f9f395a2ca632e30a194a6b97a8b
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-02 10:56:31 -0400

    Added unit tests for assigned and unassigned.

---
+++ tests/unit/todo-model-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import Todo from "ember-tdd/models/todo";
+
+module('todo');
+
+
+test('status_code of 1 is Unassigned', function(assert) {
+  var subject = Todo.create({status_code: 1});
+  assert.equal(subject.get("status"), "Unassigned");
+  subject.set("status_code", 2);
+  assert.equal(subject.get("status"), "Assigned");
+});
+
+test('status_code of 2 is Assigned', function(assert) {
+  var subject = Todo.create({status_code: 2});
+  assert.equal(subject.get("status"), "Assigned");
+});
