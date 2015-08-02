7bb98d4c9c5776c3c1de3a95deaa214332a24838
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-02 11:49:02 -0400

    Added another acceptance test and made it pass.

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -3,10 +3,10 @@ import Todo from "ember-tdd/models/todo";
 
 export default Ember.Route.extend({
   model: function() {
-    var first = Todo.create({status_code: 1, project: "first"});
-    var second = Todo.create({status_code: 1, project: "second"});
-    var third = Todo.create({status_code: 1, project: "third"});
-    var last = Todo.create({status_code: 2, project: "last"});
+    var first = Todo.create({id: 1, status_code: 1, project: "first"});
+    var second = Todo.create({id: 2, status_code: 1, project: "second"});
+    var third = Todo.create({id: 3, status_code: 1, project: "third"});
+    var last = Todo.create({id: 4, status_code: 2, project: "last"});
     return [first, second, third, last];
   }
 });
